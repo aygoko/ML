fb6e5080d926d8083800d016bfc3a533b2fed4cd
freekopik <akkoficialnyj1@gmail.com>
2025-03-23 23:03:34 +0300

    Add React Application

--- src/pages/404.js
+++
@@ -1,8 +0,0 @@
-   // pages/404.js
-   const NotFound = () => (
-    <div className="container mx-auto p-8 text-center">
-      <h1 className="text-4xl font-bold mb-4">Страница не найдена</h1>
-      <p className="text-gray-600">Проверьте URL или вернитесь на <Link to="/" className="text-primary underline">главную</Link>.</p>
-    </div>
-  );
-  
